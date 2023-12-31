70ac058379786f6fecc2a567c6f550ecf2e77436
Zach Bischoff <sgtmoloch@gmail.com>
2023-12-31 00:28:20 -0500

    UPdate to make compatible wiht RouteAi

--- InterchangeUnchained/InterchangeUnchained.cs
+++ InterchangeUnchained/InterchangeUnchained.cs
@@ -4,23 +4,11 @@ using Railloader;
 
 namespace InterchangeUnchained
 {
-    public class InterchangeUnchained : PluginBase, IUpdateHandler
+    public class InterchangeUnchained : PluginBase
     {
         public InterchangeUnchained()
         {
             new Harmony("Moloch.InterchangeUnchained").PatchAll(GetType().Assembly);
-        }
-
-        public override void OnEnable()
-        {
-        }
-
-        public override void OnDisable()
-        {
-        }
-
-        public void Update()
-        {
         }
     }
 }
